5362489502d22a9c92de19339563b7e2abaf53b2
TheBryanCastaneda <bryanccastaneda0@gmail.com>
2026-09-17 23:41:50 -0700

    Update Interpreter.java

--- set7/java/com/craftinginterpreters/lox/Interpreter.java
+++ set7/java/com/craftinginterpreters/lox/Interpreter.java
@@ -87,5 +87,34 @@ public Object visitConditionalExpr(Expr.Conditional expr) {
         return evaluate(expr.expression);
     }
 
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                checkNumberOperand(expr.operator, right);
+                return -(double)right;
+            default:
+                return null;
+        }
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) {
+            return false;
+        }
+    }
     
 }
