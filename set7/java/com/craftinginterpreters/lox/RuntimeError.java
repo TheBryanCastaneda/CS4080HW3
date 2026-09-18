284d1e8111281dd43c3a7d55c277b79d745a1124
TheBryanCastaneda <bryanccastaneda0@gmail.com>
2026-09-17 23:25:50 -0700

    runtime error done
    onto interpreter

--- set7/java/com/craftinginterpreters/lox/RuntimeError.java
+++ set7/java/com/craftinginterpreters/lox/RuntimeError.java
@@ -2,6 +2,11 @@ package com.craftinginterpreters.lox;
 
 class RuntimeError extends RuntimeException {
     private static final long serialVersionUID = 1L;
-    final Token token;
     
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
 }
