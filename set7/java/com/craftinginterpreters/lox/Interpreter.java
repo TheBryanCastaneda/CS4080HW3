89abdc13ffb4bdf1b2f2846d6071a5128bf08d95
TheBryanCastaneda <bryanccastaneda0@gmail.com>
2026-09-18 02:01:32 -0700

    Complete 7
    Oh wow I was basically at the finish line if only I had been faster or had an extra hour

--- set7/java/com/craftinginterpreters/lox/Interpreter.java
+++ set7/java/com/craftinginterpreters/lox/Interpreter.java
@@ -47,14 +47,18 @@ class Interpreter  implements Expr.Visitor<Object> {
                     return (double)left + (double)right;
                 }
 
-                if (left instanceof String && right instanceof String) {
-                    return (String)left + (String)right;
-                }
+                if (left instanceof String || right instanceof String) {
+                    return stringify(left) + stringify(right);
+}
 
-                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or include a string.");
 
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
+
+                if ((double) right == 0.0) {
+                    throw new RuntimeError(expr.operator, "Division by zero.");
+                }
                 return (double)left / (double)right;
 
             case STAR:
@@ -131,5 +135,40 @@ private boolean isEqual(Object left, Object right) {
 
     return left.equals(right);
 }
+
+private void checkNumberOperand(Token operator, Object operand) {
+    if (operand instanceof Double) {
+        return;
+    }
+
+    throw new RuntimeError(operator, "Operand must be a number.");
+}
+
+private void checkNumberOperands(
+    Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) {
+            return;
+        }
+
+    throw new RuntimeError(operator, "Operands must be numbers.");
+}
+
+private String stringify(Object object) {
+    if (object == null) {
+        return "nil";
+    }
+
+    if (object instanceof Double) {
+        String text = object.toString();
+        if (text.endsWith(".0")) {
+            text = text.substring(0, text.length() - 2);
+        }
+        return text;
+    }
+
+    return object.toString();
+}
+}
+
 
 
--- set7/java/com/craftinginterpreters/lox/Lox.java
+++ set7/java/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-  static boolean hadError = false;
+  private static final Interpreter interpreter = new Interpreter();
+
+static boolean hadError = false;
+static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -28,6 +31,9 @@ public class Lox {
 
     if (hadError) {
       System.exit(65);
+    }
+    if (hadRuntimeError) {
+      System.exit(70);
     }
   }
 
@@ -59,12 +65,18 @@ private static void run(String source) {
     return;
   }
 
-  System.out.println(new RpnPrinter().print(expression));
+  interpreter.interpret(expression);
 }
 
   static void error(int line, String message) {
     report(line, "", message);
   }
+
+  static void runtimeError(RuntimeError error) {
+  System.err.println(
+      error.getMessage() + "\n[line " + error.token.line + "]");
+  hadRuntimeError = true;
+}
 
   private static void report(int line, String where, String message) {
     System.err.println(
