7a00303b1d514acc716029172b3bc979c90dca4a
TheBryanCastaneda <bryanccastaneda0@gmail.com>
2026-09-17 23:35:51 -0700

    Update Interpreter.java

--- set7/java/com/craftinginterpreters/lox/Interpreter.java
+++ set7/java/com/craftinginterpreters/lox/Interpreter.java
@@ -50,5 +50,17 @@ class Interpreter  implements Expr.Visitor<Object> {
                 if (left instanceof String && right instanceof String) {
                     return (String)left + (String)right;
                 }
+                
+                if (left instanceof String && right instanceof Double) {
+                    return (String)left + stringify(right);
+                }
+
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+
+            case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left / (double)right;
+
+            case
         }
 }
